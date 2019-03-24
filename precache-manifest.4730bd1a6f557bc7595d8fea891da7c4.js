3854f8af447415af8fbe3b54043019382d590b19
rtonn <rtonn09umn@gmail.com>
2019-03-23 22:58:00 -0500

    Updates

---
+++ precache-manifest.4730bd1a6f557bc7595d8fea891da7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "019a48d90f12503e3dbc",
+    "url": "/clickygame/static/js/main.b5f12f1f.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/clickygame/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "019a48d90f12503e3dbc",
+    "url": "/clickygame/static/css/main.731fd3d6.chunk.css"
+  },
+  {
+    "revision": "822e0629e465dd8b33511620c9eddfa7",
+    "url": "/clickygame/index.html"
+  }
+];
