0df854230fd92e22ad69885677f8938e9be3f916
rtonn <rtonn09umn@gmail.com>
2019-03-25 18:33:07 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import pic from "./pic.json";
+// import Footer from "./components/Footer";
+
+
+//Setup Component & State.........................
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>This is a test.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  state = {
+    pic,
+    clickedPic: [],
+    score: 0
+  };
 
+//Setup click event................................
+  imageClick = event => {
+    const currentPic = event.target.alt;
+    const picAlreadyClicked =
+      this.state.clickedPic.indexOf(currentPic) > -1;
+
+    //if already picked - reset & reorder
+    if (picAlreadyClicked) {
+      this.setState({
+        pic: this.state.pic.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedPic: [],
+        score: 0
+      });
+        alert("You lose. Play again?");
+
+    //if available - score increase & reorder
+    } else {
+      this.setState(
+        {
+          pic: this.state.pic.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedPic: this.state.clickedPic.concat(
+            currentPic
+          ),
+          score: this.state.score + 1
+        },
+    //if get all - congrats & reset game        
+        () => {
+          if (this.state.score === 12) {
+            alert("You Win!");
+            this.setState({
+              pic: this.state.pic.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedPic: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+
+
+//Setup to render page................................
+render() {
+  return (
+    <div>
+      <Navbar 
+        score={this.state.score}
+      />
+      <Jumbotron />
+      <div className="wrapper">
+        {this.state.pic.map(pic => (
+          <FriendCard
+            imageClick={this.imageClick}
+            id={pic.id}
+            key={pic.id}
+            image={pic.image}
+          />
+        ))}
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+}
 export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./components.css";
+
+const Jumbotron = () => (
+<header className = "header">
+<h1>The Clicky Game!</h1>
+<h2>Click on any image to earn a point, but don't select the same image twice or you lose!</h2>
+</header>
+); 
+
+export default Jumbotron;
